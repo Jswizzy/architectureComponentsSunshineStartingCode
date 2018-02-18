f92b8fdaa6f9744f17d368fcd5f234b143556770
jswizzy <smithjustinn@hotmail.com>
2018-02-18 12:00:49 -0500

    Initial commit

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="5cecff0f-c911-486e-b1eb-b96b895a9efb")
+public class DataBindingInfo {}
